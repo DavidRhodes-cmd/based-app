4586527c2dc625d4ba96387844530c8f9b829f8e
DavidRhodes-cmd <dj.rhodes@icloud.com>
2026-08-06 18:46:47 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'based-v1';
+const CACHE = 'based-v2';
 const ASSETS = ['/', '/index.html', '/manifest.json'];
 
 self.addEventListener('install', e => {
@@ -15,6 +15,10 @@ self.addEventListener('activate', e => {
 
 self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(e.request).then(cached => cached || fetch(e.request).catch(() => caches.match('/index.html')))
+    fetch(e.request).then(fresh => {
+      const copy = fresh.clone();
+      caches.open(CACHE).then(c => c.put(e.request, copy));
+      return fresh;
+    }).catch(() => caches.match(e.request).then(cached => cached || caches.match('/index.html')))
   );
 });
